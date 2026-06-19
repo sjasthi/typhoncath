203f8b1bf9760b604dfb201c2766ca06226919b4
trevorreedy <trevinator001@gmail.com>
2026-06-19 17:36:12 -0500

    updated the rfq pipeline ui to display mock data inserted from seed.sql

--- src/app/Modules/RFQ/RFQRepository.php
+++ src/app/Modules/RFQ/RFQRepository.php
@@ -5,9 +5,22 @@ use App\Core\Database;
 
 class RFQRepository
 {
-    public function all(): array
-    {
+
+    public function all(): array{   
         // TODO: Replace with module-specific prepared queries.
-        return [];
+        $db = Database::connection();
+        $stmt = $db->prepare("SELECT * FROM rfqs ORDER BY created_at DESC");
+        $stmt->execute();
+        return $stmt->fetchAll();
     }
+
+
+    public function find_by_id($id){
+        $db = Database::connection();
+        // IMPORTANT THE '?' PREVENT SQL INJECTION AND ALLOWS FOR US TO CHECK THE 
+        // VALUE PASSED IN BEFORE RUNNING IT ON THE DB
+        $stmt = $db->prepare("SELECT FROM rfqs WHERE id==?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }   
 }

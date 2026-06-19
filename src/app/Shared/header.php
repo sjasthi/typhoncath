728e85ab85469dabe37b5cd68db686527aaefb7f
trevorreedy <trevinator001@gmail.com>
2026-06-19 12:26:01 -0500

    made architectural changes to the global.css

--- src/app/Shared/header.php
+++ src/app/Shared/header.php
@@ -16,4 +16,5 @@ Instead, the module view should include the shared header. -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="/assets/css/global.css" rel="stylesheet">
 </head>
-<body>
+    
+</div>

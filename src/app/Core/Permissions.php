496b1788c7f5f791b0e5cf9c809977f133178f42
casey-cunningham <caseycunningham99@gmail.com>
2026-06-29 12:37:26 -0500

    reverting back permissions

--- src/app/Core/Permissions.php
+++ src/app/Core/Permissions.php
@@ -38,15 +38,12 @@ class Permissions
             return false;
         }
 
-        $role = $user['role'] ?? '';
-        $map  = require __DIR__ . '/../../config/permissions.php';
-        $rolePermissions = $map[$role] ?? [];
-
-        if (in_array('*', $rolePermissions, true)) {
+        if (($user['role'] ?? '') === 'Admin' || ($user['role'] ?? '') === 'Super Admin') {
             return true;
         }
 
-        return in_array($permission, $rolePermissions, true);
+        // TODO: Load permission map from config/permissions.php.
+        return false;
     }
 
     public static function require(string $permission): void
